fa9918076a028641cab06b3ff596232cb96e3cdd
Damien de Lartigue <damien.delartigue@ppb.com>
2022-02-21 13:51:37 +0000

    create large number generator

---
+++ src/main/java/com/countdown/generator/LargeNumbers.java
@@ -0,0 +1,22 @@
+package com.countdown.generator;
+
+import java.util.Random;
+
+public class LargeNumbers {
+    final private Random random = new Random();
+    final private int[] large = {25, 50, 75, 100};
+
+    public int[] generate(){
+        int[] largeNumbers = new int[quantity()];
+
+        for (int i = 0; i < largeNumbers.length; i++) {
+            largeNumbers[i] = large[random.nextInt(4)];
+        }
+
+        return largeNumbers;
+    }
+
+    private int quantity() {
+        return random.nextInt(4) + 1;
+    }
+}
