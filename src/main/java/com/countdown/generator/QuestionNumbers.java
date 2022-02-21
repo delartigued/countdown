7f75ac9faca125a4cd48c9b182485cc44c8d85a8
Damien de Lartigue <damien.delartigue@ppb.com>
2022-02-21 13:54:54 +0000

    add logic to randomise large and small numbers

--- src/main/java/com/countdown/generator/QuestionNumbers.java
+++ src/main/java/com/countdown/generator/QuestionNumbers.java
@@ -1,9 +1,41 @@
 package com.countdown.generator;
 
+import java.util.Random;
+
 public class QuestionNumbers {
-    public static int[] generateNumbers(int target) {
-        int[] numbers = new int[6];
+    private final LargeNumbers largeNumberGenerator = new LargeNumbers();
+    private final SmallNumbers smallNumberGenerator = new SmallNumbers();
+    private final Random random = new Random();
+    private final int[] numbers = new int[6];
+
+    public QuestionNumbers(){
+    }
+
+    public int[] generate() {
+        int[] largeNumbers = largeNumberGenerator.generate();
+
+        int[] smallNumbers = smallNumberGenerator.generate(6 - largeNumbers.length);
+
+        return buildNumbersArr(largeNumbers, smallNumbers);
+    }
+
+    private int[] buildNumbersArr(int[] largeNumbers, int[] smallNumbers) {
+        int largeNumberLength = largeNumbers.length - 1;
+        int smallNumberLength = smallNumbers.length - 1;
+
+        for (int i = 0; i < numbers.length; i++) {
+            int selection = random.nextInt(2);
+            if((selection == 0 && largeNumberLength >= 0) || (selection == 1 && smallNumberLength < 0)){
+                numbers[i] = largeNumbers[largeNumberLength];
+                largeNumberLength -= 1;
+            } else if((selection == 0 && largeNumberLength < 0) || (selection == 1 && smallNumberLength >= 0)) {
+                numbers[i] = smallNumbers[smallNumberLength];
+                smallNumberLength -= 1;
+            }
+        }
 
         return numbers;
     }
+
+
 }
